876ab55cdf6eab6fa2b28fa2c33ad520cfdf296c
momoandbanana <momo_and_banana@yahoo.co.jp>
2026-04-12 20:21:00 +0900

    chore: バージョンを0.1.8に更新

--- src/file_size_splitter/__init__.py
+++ src/file_size_splitter/__init__.py
@@ -2,5 +2,5 @@
 
 from .splitter import calculate_sha512, parse_size, split_file, generate_bat_script, generate_ps1_script
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 __all__ = ["calculate_sha512", "parse_size", "split_file", "generate_bat_script", "generate_ps1_script"]
